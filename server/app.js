1ee067e9adde60459342307b726870f21652624d
Himel-010 <eakram68@gmail.com>
2026-05-08 18:52:36 +0600

    updates

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "https://law-link-bd.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
